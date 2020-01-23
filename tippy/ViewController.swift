4efa3151f57b71601f5774316f98124551047a8f
Juan Zuniga <jzunigaruiz@csumb.edu>
2020-01-22 17:10:16 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Juan Zuniga on 1/22/20.
+//  Copyright © 2020 Juan Zuniga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
